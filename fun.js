6f143d1dc1dede5008f29fc302f272dfe97dd1f1
bidursapkota00 <bidursapkota00@gmail.com>
2024-04-11 10:50:50 +0545

    js for react

---
+++ fun.js
@@ -0,0 +1,17 @@
+console.log(1 + 1);
+console.log("1" + 1);
+console.log("1" - 1);
+
+const myName = "bidur";
+console.log(myName.toUpperCase());
+
+const number = 1;
+console.log(number.toString());
+
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+console.log(greet("Alice"));
+
+greet.language = "English";
+console.log(greet.language);
