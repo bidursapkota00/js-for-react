6ff95fabd671125d78fac93f91d521a11d35711e
bidursapkota00 <bidursapkota00@gmail.com>
2024-04-19 10:31:18 +0545

    prep

---
+++ script1.js
@@ -0,0 +1,33 @@
+const book = {
+  id: 1,
+  title: "The Lord of the Rings",
+  publicationDate: "1954-07-29",
+  author: "J. R. R. Tolkien",
+  genres: [
+    "fantasy",
+    "high-fantasy",
+    "adventure",
+    "fiction",
+    "novels",
+    "literature",
+  ],
+  hasMovieAdaptation: true,
+  pages: 1216,
+  translations: {
+    spanish: "El señor de los anillos",
+    chinese: "魔戒",
+    french: "Le Seigneur des anneaux",
+  },
+  reviews: {
+    goodreads: {
+      rating: 4.52,
+      ratingsCount: 630994,
+      reviewsCount: 13417,
+    },
+    librarything: {
+      rating: 4.53,
+      ratingsCount: 47166,
+      reviewsCount: 452,
+    },
+  },
+};
